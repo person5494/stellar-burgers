1b8017764e0fd765a68eccb05944bf8c2f27fa14
Alexey S. <person432@ya.ru>
2026-07-19 20:21:51 +0300

    Set up Redux store

---
+++ src/services/slices/ingredientsSlice.ts
@@ -0,0 +1,23 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+import type { TIngredient } from '@utils-types';
+
+type TIngredientsState = {
+  ingredients: TIngredient[];
+  isLoading: boolean;
+  error: string | null;
+};
+
+const initialState: TIngredientsState = {
+  ingredients: [],
+  isLoading: false,
+  error: null
+};
+
+const ingredientsSlice = createSlice({
+  name: 'ingredients',
+  initialState,
+  reducers: {}
+});
+
+export const ingredientsReducer = ingredientsSlice.reducer;
