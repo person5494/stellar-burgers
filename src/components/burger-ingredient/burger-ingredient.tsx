8160e5be3e0558b254216603dcfdc0b17421d432
Alexey S. <person432@ya.ru>
2026-07-20 16:52:28 +0300

    Add ingredients to constructor

--- src/components/burger-ingredient/burger-ingredient.tsx
+++ src/components/burger-ingredient/burger-ingredient.tsx
@@ -2,13 +2,20 @@ import { FC, memo } from 'react';
 import { useLocation } from 'react-router-dom';
 
 import { BurgerIngredientUI } from '@ui';
+
+import { addIngredient } from '../../services/slices/burgerConstructorSlice';
+import { useDispatch } from '../../services/store';
+
 import { TBurgerIngredientProps } from './type';
 
 export const BurgerIngredient: FC<TBurgerIngredientProps> = memo(
   ({ ingredient, count }) => {
     const location = useLocation();
+    const dispatch = useDispatch();
 
-    const handleAdd = () => {};
+    const handleAdd = () => {
+      dispatch(addIngredient(ingredient));
+    };
 
     return (
       <BurgerIngredientUI
