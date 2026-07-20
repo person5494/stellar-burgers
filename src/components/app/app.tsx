c63d040067f6b1bba68aedcd18b52d4598ec1a87
Alexey S. <person432@ya.ru>
2026-07-20 18:45:56 +0300

    Add ingredient details modal routing

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,5 +1,5 @@
 import { useEffect } from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, useLocation, useNavigate } from 'react-router-dom';
 
 import {
   ConstructorPage,
@@ -17,7 +17,7 @@ import '../../index.css';
 
 import styles from './app.module.css';
 
-import { AppHeader } from '@components';
+import { AppHeader, IngredientDetails, Modal } from '@components';
 import { Preloader } from '@ui';
 
 import {
@@ -26,10 +26,21 @@ import {
   selectIngredientsError,
   selectIngredientsLoading
 } from '../../services/slices/ingredientsSlice';
+
 import { useDispatch, useSelector } from '../../services/store';
+
+type TLocationState = {
+  background?: Location;
+};
 
 const App = () => {
   const dispatch = useDispatch();
+
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  const state = location.state as TLocationState | null;
+  const background = state?.background;
 
   const isIngredientsLoading = useSelector(selectIngredientsLoading);
   const ingredients = useSelector(selectIngredients);
@@ -38,6 +49,10 @@ const App = () => {
   useEffect(() => {
     dispatch(getIngredients());
   }, [dispatch]);
+
+  const handleModalClose = () => {
+    navigate(-1);
+  };
 
   const constructorPage = isIngredientsLoading ? (
     <Preloader />
@@ -57,7 +72,7 @@ const App = () => {
     <div className={styles.app}>
       <AppHeader />
 
-      <Routes>
+      <Routes location={background || location}>
         <Route path='/' element={constructorPage} />
         <Route path='/feed' element={<Feed />} />
         <Route path='/login' element={<Login />} />
@@ -66,8 +81,23 @@ const App = () => {
         <Route path='/reset-password' element={<ResetPassword />} />
         <Route path='/profile' element={<Profile />} />
         <Route path='/profile/orders' element={<ProfileOrders />} />
+        <Route path='/ingredients/:id' element={<IngredientDetails />} />
+        <Route path='/404' element={<NotFound404 />} />
         <Route path='*' element={<NotFound404 />} />
       </Routes>
+
+      {background && (
+        <Routes>
+          <Route
+            path='/ingredients/:id'
+            element={
+              <Modal title='Детали ингредиента' onClose={handleModalClose}>
+                <IngredientDetails />
+              </Modal>
+            }
+          />
+        </Routes>
+      )}
     </div>
   );
 };
--- src/components/ingredient-details/ingredient-details.tsx
+++ src/components/ingredient-details/ingredient-details.tsx
@@ -1,13 +1,35 @@
 import { FC } from 'react';
-import { Preloader } from '../ui/preloader';
-import { IngredientDetailsUI } from '../ui/ingredient-details';
+import { Navigate, useParams } from 'react-router-dom';
+import { IngredientDetailsUI, Preloader } from '@ui';
+
+import {
+  selectIngredients,
+  selectIngredientsError,
+  selectIngredientsLoading
+} from '../../services/slices/ingredientsSlice';
+import { useSelector } from '../../services/store';
 
 export const IngredientDetails: FC = () => {
-  /** TODO: взять переменную из стора */
-  const ingredientData = null;
+  const { id } = useParams();
+
+  const ingredients = useSelector(selectIngredients);
+  const isIngredientsLoading = useSelector(selectIngredientsLoading);
+  const error = useSelector(selectIngredientsError);
+
+  const ingredientData = ingredients.find(
+    (ingredient) => ingredient._id === id
+  );
+
+  if (isIngredientsLoading) {
+    return <Preloader />;
+  }
+
+  if (error) {
+    return <Navigate to='/404' replace />;
+  }
 
   if (!ingredientData) {
-    return <Preloader />;
+    return <Navigate to='/404' replace />;
   }
 
   return <IngredientDetailsUI ingredientData={ingredientData} />;
