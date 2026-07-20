74fbfb50bd853db39bc7a36486db8e6825799387
Alexey S. <person432@ya.ru>
2026-07-20 17:37:02 +0300

    Add constructor item controls

--- src/components/burger-constructor-element/burger-constructor-element.tsx
+++ src/components/burger-constructor-element/burger-constructor-element.tsx
@@ -1,14 +1,40 @@
 import { FC, memo } from 'react';
+
 import { BurgerConstructorElementUI } from '@ui';
+
+import {
+  moveIngredient,
+  removeIngredient
+} from '../../services/slices/burgerConstructorSlice';
+import { useDispatch } from '../../services/store';
+
 import { BurgerConstructorElementProps } from './type';
 
 export const BurgerConstructorElement: FC<BurgerConstructorElementProps> = memo(
   ({ ingredient, index, totalItems }) => {
-    const handleMoveDown = () => {};
+    const dispatch = useDispatch();
 
-    const handleMoveUp = () => {};
+    const handleMoveDown = () => {
+      dispatch(
+        moveIngredient({
+          fromIndex: index,
+          toIndex: index + 1
+        })
+      );
+    };
 
-    const handleClose = () => {};
+    const handleMoveUp = () => {
+      dispatch(
+        moveIngredient({
+          fromIndex: index,
+          toIndex: index - 1
+        })
+      );
+    };
+
+    const handleClose = () => {
+      dispatch(removeIngredient(ingredient.id));
+    };
 
     return (
       <BurgerConstructorElementUI
