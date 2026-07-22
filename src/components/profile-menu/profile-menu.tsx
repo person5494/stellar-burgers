0bb0ee2b1d61a87faa976f128d30ee2370381cef
Alexey S. <person432@ya.ru>
2026-07-22 13:47:01 +0300

    Add user logout

--- src/components/profile-menu/profile-menu.tsx
+++ src/components/profile-menu/profile-menu.tsx
@@ -1,11 +1,21 @@
 import { FC } from 'react';
-import { useLocation } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 import { ProfileMenuUI } from '@ui';
 
+import { logoutUser } from '../../services/slices/userSlice';
+import { useDispatch } from '../../services/store';
+
 export const ProfileMenu: FC = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
   const { pathname } = useLocation();
 
-  const handleLogout = () => {};
+  const handleLogout = async () => {
+    try {
+      await dispatch(logoutUser()).unwrap();
+      navigate('/login', { replace: true });
+    } catch {}
+  };
 
   return <ProfileMenuUI handleLogout={handleLogout} pathname={pathname} />;
 };
