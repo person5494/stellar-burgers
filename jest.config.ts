f1f17405c2e33d68f59d469a9c9706b65a326130
Alexey S. <person432@ya.ru>
2026-08-05 13:40:31 +0300

    Configure Jest

---
+++ jest.config.ts
@@ -0,0 +1,19 @@
+import type { JestConfigWithTsJest } from 'ts-jest';
+
+const config: JestConfigWithTsJest = {
+  transform: {
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      {
+        tsconfig: 'tsconfig.json'
+      }
+    ]
+  },
+  testEnvironment: 'node',
+  moduleNameMapper: {
+    '^@api$': '<rootDir>/src/utils/burger-api.ts',
+    '^@utils-types$': '<rootDir>/src/utils/types'
+  }
+};
+
+export default config;
